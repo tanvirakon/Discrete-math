f0717ee92cb414d59b8a49bdff08bfc8ac8c5aeb
tanvirakon <87146763+tanvirakon@users.noreply.github.com>
2023-06-06 11:02:53 +0600

    Add files via upload

--- lab5/a.cpp
+++ lab5/a.cpp
@@ -1,80 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long int
+#define endl '\n'
+#define nn 100009
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define f0(n) for (int i = 0; i < n; i++)
+#define f1(n) for (int i = 1; i < n; i++)
 #define all(_a) _a.begin(), _a.end()
-
-int req(int a,int b){
-    int s,t;
-    if(b==0){
-        s=1;
-        t=0;
-
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+#define fast()                            \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(NULL);                    \
     }
-    else{
-
+void vscodeonly()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+void errortxt()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+}
+void yoyochikichiki()
+{
+    fast();
+    vscodeonly();
+    errortxt();
+}
+void primefactorization(int n)
+{
+    set<int> set;
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+            {
+                n /= i;
+                set.insert(i);
+            }
+        }
     }
+    for (auto i : set)
+        cout << i << " ";
 }
 
 int32_t main()
 {
-    // yoyochikichiki();
-    int a, b;
-    cin >> a >> b;
-    int c, d;
-    c = a;
-    d = b;
-    vector<int> a1;
-    vector<int> a2;
-    while (a != 1)
-    {
-        for (int i = 2; i <= a; i++)
-        {
-            if (a % i == 0)
-            {
-                a1.push_back(i);
-                a /= i;
-                break;
-            }
-        }
-    }
-
-    for (int i = 0; i < a1.size(); i++)
-        cout << a1[i] << " ";
-    cout << endl;
-    while (b != 1)
-    {
-        for (int i = 2; i <= b; i++)
-        {
-            if (b % i == 0)
-            {
-                a2.push_back(i);
-                b /= i;
-                break;
-            }
-        }
-    }
-
-    for (int i = 0; i < a2.size(); i++)
-        cout << a2[i] << " ";
-    cout << endl;
-
-    int sum = 1;
-    for (int i = 0; i < a1.size(); i++)
-    {
-
-        for (int j = 0; j < a2.size(); j++)
-        {
-            auto it = find(all(a2), a1[i]);
-            if (it != a2.end())
-            {
-                sum *= a1[i];
-                a2.erase(it);
-                break;
-            }
-        }
-    }
-    cout << "gcd " << sum << endl;
-    cout << "lcm " << (c * d) / sum << endl;
-
-
-
+    yoyochikichiki();
+    int n;
+    cin >> n;
+    primefactorization(n);
 }
